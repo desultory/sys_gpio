96d6b7a7470f2ee89ef81aace5349c549ef8d181
Zen <z@pyl.onl>
2024-12-15 13:38:23 -0600

    add more cli commands, fix import
    Signed-off-by: Zen <z@pyl.onl>

---
+++ src/sys_gpio/cli_commands.py
@@ -0,0 +1,24 @@
+from sys_gpio import Pin
+from zenlib.util import get_kwargs
+
+
+def get_pin_value():
+    args = [
+            {"flags": ["pin"], "help": "Pin to get value from", "action": "store", "type": int},
+    ]
+
+    kwargs = get_kwargs(package="sys_gpio", description="get pin value", arguments=args)
+    pin = Pin(**kwargs)
+    print(pin.value)
+
+def get_pin_direction():
+    args = [
+            {"flags": ["pin"], "help": "Pin to get direction from", "action": "store", "type": int},
+    ]
+
+    kwargs = get_kwargs(package="sys_gpio", description="get pin direction", arguments=args)
+    pin = Pin(**kwargs)
+    print(pin.direction)
+
+def get_pin_exports():
+    print("Exported pins:" + ", ".Pin.get_exports())
